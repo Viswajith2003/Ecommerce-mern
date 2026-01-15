dc32ee923df0480e3f0df2a1dca32a5eaf76dbe9
Viswajith2003 <viswajithviswa715@gmail.com>
2026-01-15 23:48:42 +0530

    cors issue solved

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,25 @@
-import ConnectDB from "./config/db.js"
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import router from "./routes/ProductRoutes.js"
+import ConnectDB from "./config/db.js";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import router from "./routes/ProductRoutes.js";
 
 dotenv.config();
 
-const app=express();
-app.use('/uploads', express.static('uploads'));
+const app = express();
+app.use("/uploads", express.static("uploads"));
 
-
-app.use(express.json())
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true,
-        methods:["POST","GET"],
-    }
-))
-app.use("/api/products",router)
-
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://ecommerce-mern-roan.vercel.app",
+    credentials: true,
+    methods: ["POST", "GET"],
+  })
+);
+app.use("/api/products", router);
 
 ConnectDB();
 
-
-
-const PORT=process.env.PORT || 5001
-app.listen(PORT,()=>console.log(`Server running on ${PORT}`))
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
